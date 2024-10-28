aa3b38f1159a6c8dc0a01f41dd78c447888c3d5a
Brenda Yañez <brendayw97@gmail.com>
2024-10-28 17:33:54 -0300

    api test

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-axios.get('https://magicloops.dev/api/loop/run/e4e7ff5b-1392-45b6-91e8-06d1b8154b8b?')
+axios.get('https://formspree.io/f/xeoqevzl')
     .then(response => {
         console.log('Data retrieved from the API:', response.data)
     })
